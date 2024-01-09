d08180635e0e45ecb60c2c756e96a774145b46dc
Steve Paul <stevep@interrait.com>
2024-01-09 19:39:49 +0530

    Add contact-manager component

---
+++ src/app/components/contact-manager/contact-manager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-manager',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-manager.component.html',
+  styleUrl: './contact-manager.component.scss'
+})
+export class ContactManagerComponent {
+
+}
