264a40872146a86b072ec8b943fc89ee22e12228
Steve Paul <stevep@interrait.com>
2024-01-09 19:45:47 +0530

    Add necessary routes for crud operations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ContactManagerComponent } from './components/contact-manager/contact-manager.component';
+import { AddContactComponent } from './components/add-contact/add-contact.component';
+import { EditContactComponent } from './components/edit-contact/edit-contact.component';
+import { ViewContactComponent } from './components/view-contact/view-contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/contacts/admin', pathMatch: 'full'},
+  {path: 'contacts/admin', component: ContactManagerComponent},
+  {path: 'contacts/add', component: AddContactComponent},
+  {path: 'contacts/edit/:contactId', component: EditContactComponent},
+  {path: 'contacts/view/:contactId', component: ViewContactComponent},
+];
