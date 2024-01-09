a6490b68363cda29f197f05e619176e2a8136dc2
Steve Paul <stevep@interrait.com>
2024-01-09 23:01:43 +0530

    Update edit contact component

--- src/app/components/edit-contact/edit-contact.component.ts
+++ src/app/components/edit-contact/edit-contact.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { SpinnerComponent } from '../spinner/spinner.component';
+import { IContact } from '../../models/IContact';
+import { ContactService } from '../../services/contact.service';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-contact',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SpinnerComponent, NgIf, FormsModule],
   templateUrl: './edit-contact.component.html',
   styleUrl: './edit-contact.component.scss'
 })
 export class EditContactComponent {
+  public loading: boolean = false;
+  public contactId: string | null = null;
+  public contact: IContact = {} as IContact;
+  public errorMessage: string | null = null;
 
+  constructor(private activatedRoute: ActivatedRoute, private contactService: ContactService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.activatedRoute.paramMap.subscribe((param) => {
+      this.contactId = param.get('contactId');
+    });
+    if (this.contactId) {
+      this.contactService.getContact(this.contactId).subscribe((data) => {
+        this.contact = data;
+        this.loading = false;
+      }, (error) => {
+        this.errorMessage = error;
+        this.loading = false;
+      })
+    }
+  }
+
+  submitUpdate() {
+    if (this.contactId) {
+        this.contactService.updateContact(this.contact, this.contactId).subscribe((data) => {
+        this.router.navigate(['/']).then();
+      }, (error) => {
+        this.errorMessage = error;
+        this.router.navigate([`contacts/edit/${this.contactId}`]).then();
+      })
+    }
+  }
 }
