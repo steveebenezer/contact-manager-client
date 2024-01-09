5a50dc83b56b358b4f8a7b7a78078ef20d34d1cc
Steve Paul <stevep@interrait.com>
2024-01-09 20:39:59 +0530

    Update edit-contact component

--- src/app/components/edit-contact/edit-contact.component.ts
+++ src/app/components/edit-contact/edit-contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-edit-contact',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './edit-contact.component.html',
   styleUrl: './edit-contact.component.scss'
 })
