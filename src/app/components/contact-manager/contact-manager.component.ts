c657963d27ee852b60feb23513aca849350c852c
Steve Paul <stevep@interrait.com>
2024-01-09 21:57:18 +0530

    Update contact manager component to get data from api

--- src/app/components/contact-manager/contact-manager.component.ts
+++ src/app/components/contact-manager/contact-manager.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { IContact } from '../../models/IContact';
+import { ContactService } from '../../services/contact.service';
+import { NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-contact-manager',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgIf, NgForOf],
   templateUrl: './contact-manager.component.html',
   styleUrl: './contact-manager.component.scss'
 })
 export class ContactManagerComponent {
+  public loading: boolean = false;
+  public contacts: IContact[] = [];
+  public errorMessage: string | null = null;
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.contactService.getAllContacts()
+    .subscribe((data) => {
+      this.contacts = data;
+      this.loading = false;
+    }, (error) => {
+      this.errorMessage = error;
+      this.loading = false;
+    });
+  }
+
 
 }
