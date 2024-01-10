106d1996f79154c58eea55c885f652be248efcb8
Steve P <stevep@interrait.com>
2024-01-11 00:14:38 +0530

    Remove view component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { ContactManagerComponent } from './components/contact-manager/contact-manager.component';
 import { AddContactComponent } from './components/add-contact/add-contact.component';
 import { EditContactComponent } from './components/edit-contact/edit-contact.component';
-import { ViewContactComponent } from './components/view-contact/view-contact.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
@@ -10,6 +9,5 @@ export const routes: Routes = [
   {path: 'contacts/admin', component: ContactManagerComponent},
   {path: 'contacts/add', component: AddContactComponent},
   {path: 'contacts/edit/:contactId', component: EditContactComponent},
-  {path: 'contacts/view/:contactId', component: ViewContactComponent},
   {path: '**', component: PageNotFoundComponent},
 ];
