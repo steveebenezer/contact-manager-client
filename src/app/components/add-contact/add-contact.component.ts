9e118393b1b98133ec011d7f971092e50cc32e92
Steve Paul <stevep@interrait.com>
2024-01-10 21:43:31 +0530

    Update add contact component to use reactive forms

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -2,7 +2,7 @@ import { Component, NgModule, Input, Output } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { IContact } from '../../models/IContact';
 import { ContactService } from '../../services/contact.service';
-import { NgModel, FormsModule } from '@angular/forms';
+import { NgModel, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { SpinnerComponent } from '../spinner/spinner.component';
 import { NgIf } from '@angular/common';
 
@@ -13,27 +13,43 @@ NgModule({
 @Component({
   selector: 'app-add-contact',
   standalone: true,
-  imports: [RouterModule, FormsModule, SpinnerComponent, NgIf],
+  imports: [RouterModule, FormsModule, SpinnerComponent, NgIf, ReactiveFormsModule],
   templateUrl: './add-contact.component.html',
   styleUrl: './add-contact.component.scss'
 })
 export class AddContactComponent {
   public loading: boolean = false;
   public contactId: string | null = null;
+  public contactForm: FormGroup;
   public contact: IContact = {} as IContact;
   public errorMessage: string | null = null;
 
-  constructor(private contactService: ContactService, private router: Router) {}
+  constructor(private contactService: ContactService, private router: Router, private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
   }
 
   public createSubmit() {
-    this.contactService.createContact(this.contact).subscribe((data) => {
-      this.router.navigate(['/']).then();
-    }, (error) => {
-      this.errorMessage = error;
-      this.router.navigate(['contacts/add']).then();
-    })
+    if (this.contactForm.valid) {
+      this.loading = true;
+      const newContact: IContact = this.contactForm.value;
+      this.contactService.createContact(newContact).subscribe(
+        (data) => {
+          this.router.navigate(['/']).then(() => {
+            this.loading = false;
+          });
+        },
+        (error) => {
+          this.errorMessage = error;
+          this.loading = false;
+        }
+      );
+    }
   }
 }
